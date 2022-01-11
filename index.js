8e413d7cb5ad1e664ba485d69cc478ab93dc7831
D.Loman <donnaloman@gmail.com>
2022-01-11 15:03:29 +0000

    adding neils explanation

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 require ('dotenv').config();
 const {Sequelize, DataTypes} = require('sequelize');
-
 const connection = new Sequelize (process.env.CC_DB);
 
-const Test = connection.define("tests",
-{
-    name:{
-        type: DataTypes.STRING
-    }
-})
+// const Test = connection.define("tests",
+// {
+//     name:{
+//         type: DataTypes.STRING
+//     }
+// })
 
-const test = async () => {
+// const test = async () => {
+//     try {
+//         await connection.authenticate();
+//         await Test.sync({alter: true})
+//         await Test.create({name: "bob"});
+
+
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+
+// test();
+
+
+const main = async() => {
     try {
         await connection.authenticate();
-        await Test.sync({alter: true})
-        await Test.create({name: "bob"});
+        console.log('Connection has been succesfully established')
 
-
-    } catch (err) {
-        console.log(err);
+    } catch(err) {
+        console.error("unable to connect to database", err)
     }
+
+    await connection.close();
+    process.exit();
+
 }
 
-test();
+main()
+
+//______________________________________________________________________
+
---
+++ index2.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require("require");
+
+const connection = new Sequelize("master32", "root", "password", {
+    host: "localhost",
+    dialect: "mysql",
+
+});
+
+const main = async() => {
+    try {
+        await connection.authenticate();
+        console.log('Connection has been succesfully established')
+
+    } catch(err) {
+        console.error("unable to connect to database", err)
+    }
+
+    await connection.close();
+    process.exit();
+
+}
+
+main()
